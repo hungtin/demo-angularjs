5be42bc3a0a6560dafd2a5d4f2b99d2aed254dce
Tin Trinh <trinh.tin@hamee.co.jp>
2018-10-22 09:54:47 +0900

    add dashboard route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,12 +14,18 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: click hero '
+      + 'id ' + hero.id + ' name ' + hero.name);
   }
-  constructor() {
-    this.heroes = HEROES;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
